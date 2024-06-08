cfad3b8ffdf744f851a1a76306de5235ea95dc3a
Steamymoomilk <142749239+Steamymoomilk@users.noreply.github.com>
2024-06-08 17:40:14 -0400

    Add files via upload

---
+++ config/scripts/extra-packages.sh
@@ -0,0 +1,6 @@
+
+brew install --cask vscodium
+brew install yazi ffmpegthumbnailer unar jq poppler fd ripgrep fzf zoxide font-symbols-only-nerd-font atuin bat
+ujust toggle-updates off
+ujust setup-distrobox-git
+ujust install-obs-studio-portable
