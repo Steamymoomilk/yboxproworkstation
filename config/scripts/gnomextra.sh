5e3e5e74b8b9e83aecdfcf7aba2536c1c2c04772
Steamymoomilk <142749239+Steamymoomilk@users.noreply.github.com>
2024-06-09 12:10:11 -0400

    Create gnomextra.sh

---
+++ config/scripts/gnomextra.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+gnome-extensions enable monitor@astraext.github.io
+gnome-extensions enable blur-my-shell@aunetx
+gnome-extensions enable caffeine@patapon.info
+gnome-extensions enable runcat@kolesnikov.se
+gnome-extensions enable user-theme@gnome-shell-extensions.gcampax.github.com
