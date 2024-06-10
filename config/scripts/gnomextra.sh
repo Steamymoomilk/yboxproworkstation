08e4db29b4b45e69bed5801b23d843906c795ef3
Steamymoomilk <142749239+Steamymoomilk@users.noreply.github.com>
2024-06-09 20:15:11 -0400

    Update gnomextra.sh

--- config/scripts/gnomextra.sh
+++ config/scripts/gnomextra.sh
@@ -6,6 +6,6 @@ gnome-extensions enable monitor@astraext.github.io
 gnome-extensions enable caffeine@patapon.info
 gnome-extensions enable user-theme@gnome-shell-extensions.gcampax.github.com
 #mkdir
-mkdir /usr/share/ublue-os/firstboot/
-/usr/bin/yafti /usr/share/ublue-os/firstboot/yafti.yml
-mkdir /usr/share/ublue-os/firstboot/launcher/
+#mkdir /usr/share/ublue-os/firstboot/
+#/usr/bin/yafti /usr/share/ublue-os/firstboot/yafti.yml
+#mkdir /usr/share/ublue-os/firstboot/launcher
